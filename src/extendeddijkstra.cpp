7ab2ffa269e538645762a07660bc94205afeab4c
jackyEightz <jackyeightzhang@gmail.com>
2019-10-29 18:01:09 -0700

    extendeddijkstra v1

--- src/extendeddijkstra.cpp
+++ src/extendeddijkstra.cpp
@@ -3,6 +3,7 @@
 #include <utility>
 #include <set>
 #include <iostream>
+#include <limits.h>
 using namespace std;
 
 typedef char* Node;
@@ -10,12 +11,18 @@ typedef map<char*,pair<char*,long>> Graph;
 
 map<Node, long>  extendedDijkstra( Node source, Graph graph ){
 	map<Node, set<Node>> node_map;
+	for(auto it : graph) { //initialize node_map with all the nodes and their reachable edges
+		node_map[it.first].insert(it.second.first);
+	}
 	map<pair<Node,Node>, long> cost_map;
 	map<Node, long> answer_map;
 	map<Node, long> info_map;
-	cout << "passed" << endl;
-	//map<info_map: KEY=Node, VALUE=Info(INFINITY)
-	//info_map[source] = 0
+	cout << "data structures initialized" << endl;
+	for(auto it : node_map){		//initialize info_map with all nodes and with Info to infinity
+		info_map[it.first] = LONG_MAX;
+	}
+	info_map[source] = 0;		//set source node distance to 0
+
 	//info_pq : pair(Node, Info)
 	//sorted by smallest distance 
 	//
